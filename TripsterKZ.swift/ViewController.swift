8ddb1834fac397bcbcae2f9da335e8ee3a6588e3
dakosh <230107034@sdu.edu.kz>
2025-12-14 22:33:09 +0500

    Initial Commit

---
+++ TripsterKZ.swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripsterKZ.swift
+//
+//  Created by Dana Ashen on 14.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
